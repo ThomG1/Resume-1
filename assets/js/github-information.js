2cb8b19a76a11ab9eb87f1af46638e00a393ab1a
ThomG1 <thomas.gwyther@hotmail.co.uk>
2022-11-29 15:05:59 +0000

    Add github username

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,8 +1,26 @@
+function userInformationHTML (user) {
+    return `<h2>${user.name}
+    <span class="small-name>
+    (@<a href="${user.html_url}" target="_blank">${user.login}</a>
+        </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar>
+            <a href="${user.html_url} target="_blank">
+            <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
+            </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`
+}
+
+
+
 function fetchGitHubInformation(event) {
 
-    var username =$("#gh-username").val();
+    var username = $("#gh-username").val();
     if (!username) {
-        $("gh-ser-data").html(`<h2>Please enter a GitHub username</h2>`);
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
     $("#gh-user-data").html(
